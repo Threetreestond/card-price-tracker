d1f241f2dad63d912725043e2884d7820fe8ac2a
Threetreestond <tneseyifdeakin@yahoo.co.uk>
2026-04-03 15:39:03 +0100

    added get_prices() function

--- src/fetcher.py
+++ src/fetcher.py
@@ -3,17 +3,27 @@ import json
 
 SORCERY_CATEGORY_ID = 77
 
+
 def get_sorcery_groups():
     url = f"https://tcgcsv.com/tcgplayer/{SORCERY_CATEGORY_ID}/groups"
     response = requests.get(url)
     data = response.json()
     return data["results"]
 
+
 def get_products(group_id):
     url = f"https://tcgcsv.com/tcgplayer/{SORCERY_CATEGORY_ID}/{group_id}/products"
     response = requests.get(url)
     data = response.json()
     return data["results"]
+
+
+def get_prices(group_id):
+    url = f"https://tcgcsv.com/tcgplayer/{SORCERY_CATEGORY_ID}/{group_id}/prices"
+    response = requests.get(url)
+    data = response.json()
+    return data["results"]
+
 
 if __name__ == "__main__":
     ALPHA_GROUP_ID = 23335
