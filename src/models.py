4b7fc07fac53bff5faa8c3e6a44b561fd1c3d2c3
Threetreestond <tneseyifdeakin@yahoo.co.uk>
2026-04-03 22:50:42 +0100

    added add_card functionality to class and db

--- src/models.py
+++ src/models.py
@@ -4,16 +4,24 @@ class Deck:
     def __init__(self, name, deck_id=None):
         self.name = name
         self.deck_id = deck_id
-        self.cards = []
+        self.cards = {}
     
     def add_card(self, product_id, quantity=1):
-        pass
-    
+        if self.deck_id is None:
+            self.save()
+        add_card_to_deck(self.deck_id, product_id, quantity)
+        if product_id in self.cards:
+            self.cards[product_id] += quantity
+        else:
+            self.cards[product_id] = quantity
+
+
     def remove_card(self, product_id):
         pass
     
     def save(self):
-        pass
+        if self.deck_id is None:
+            self.deck_id = save_deck(self)
     
     def load(self):
         pass
