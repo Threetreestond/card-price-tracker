35365278cdcb18d7845bf305f1492773db1679fe
Threetreestond <tneseyifdeakin@yahoo.co.uk>
2026-04-06 12:03:11 +0100

    add POST /decks endpoint to create new decks

--- api.py
+++ api.py
@@ -2,8 +2,17 @@ import sys
 sys.path.insert(0, "src")
 from fastapi import FastAPI
 from database import get_all_decks
+from pydantic import BaseModel
+from models import Deck
+
 
 app = FastAPI()
+
+
+class DeckCreate(BaseModel):
+    name: str
+
+
 
 @app.get("/")
 def root():
@@ -12,3 +21,11 @@ def root():
 @app.get("/decks")
 def get_decks():
     return get_all_decks()
+
+@app.post("/decks")
+def create_deck(deck: DeckCreate):
+    # deck.name is available here
+    new_deck = Deck(name=deck.name)
+    new_deck.save()
+# plain dict — FastAPI converts this to JSON automatically
+    return {"deck_id": new_deck.deck_id, "name": new_deck.name}
